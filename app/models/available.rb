a47737cb720cc94689f65a8338bebb09d904e6c8
Raghu Betina <raghu@firstdraft.com>
2016-10-28 02:17:06 +0000

    Direct association between foods and availables

--- app/models/available.rb
+++ app/models/available.rb
@@ -1,6 +1,8 @@
 class Available < ApplicationRecord
   # Direct associations
 
+  belongs_to :foods
+
   # Indirect associations
 
   # Validations
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,11 @@
 class Food < ApplicationRecord
   # Direct associations
 
+  has_many   :pantries,
+             :class_name => "Available",
+             :foreign_key => "foods_id",
+             :dependent => :destroy
+
   has_many   :similar_foods,
              :class_name => "Similar",
              :foreign_key => "foods_id",
