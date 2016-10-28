e8730b42eca4126f81e5a7e8f1d0168af507730d
Raghu Betina <raghu@firstdraft.com>
2016-10-28 02:17:50 +0000

    Whitelist columns in Similar for ActiveAdmin

--- app/admin/similar.rb
+++ app/admin/similar.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Similar do
 
+ permit_params :similar_name, :foods_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
