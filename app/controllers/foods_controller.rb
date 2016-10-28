cf4cd20f844bbbfdcf79cce8f67b7138827e5c74
Raghu Betina <raghu@firstdraft.com>
2016-10-28 02:16:50 +0000

    Generate foods resource with starter:resource

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,48 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new
+    @food.name = params[:name]
+
+    if @food.save
+      redirect_to "/foods", :notice => "Food created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    @food = Food.find(params[:id])
+
+    @food.name = params[:name]
+
+    if @food.save
+      redirect_to "/foods", :notice => "Food updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+
+    @food.destroy
+
+    redirect_to "/foods", :notice => "Food deleted."
+  end
+end
