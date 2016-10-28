3e46a52b2fd71be913cf665aa8b9232c1cddcc71
Raghu Betina <raghu@firstdraft.com>
2016-10-28 02:17:53 +0000

    Whitelist columns in AvailablesSimilar for ActiveAdmin

--- app/admin/availables_similar.rb
+++ app/admin/availables_similar.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AvailablesSimilar do
 
+ permit_params :similar_id, :availables_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
