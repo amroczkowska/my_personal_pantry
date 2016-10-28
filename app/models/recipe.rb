13894475516aff76ddb484d1ee2c49a528c560a9
Raghu Betina <raghu@firstdraft.com>
2016-10-28 02:17:06 +0000

    Direct association between recipes and foods_recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ApplicationRecord
   # Direct associations
 
+  has_many   :foods_recipes,
+             :foreign_key => "recipes_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
