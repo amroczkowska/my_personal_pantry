e0e7502c384842c1328b76d3834edc2fb7d8d528
Raghu Betina <raghu@firstdraft.com>
2016-10-28 02:17:06 +0000

    Direct association between similars and availables_similars

--- app/models/availables_similar.rb
+++ app/models/availables_similar.rb
@@ -1,6 +1,10 @@
 class AvailablesSimilar < ApplicationRecord
   # Direct associations
 
+  belongs_to :similar_foods,
+             :class_name => "Similar",
+             :foreign_key => "similar_id"
+
   belongs_to :availables
 
   # Indirect associations
--- app/models/similar.rb
+++ app/models/similar.rb
@@ -1,6 +1,10 @@
 class Similar < ApplicationRecord
   # Direct associations
 
+  has_many   :similar_foods_availables,
+             :class_name => "AvailablesSimilar",
+             :dependent => :destroy
+
   belongs_to :foods
 
   # Indirect associations
