935bab0ef5b9e047aa3d9773e59c493c56578a21
Raghu Betina <raghu@firstdraft.com>
2016-11-27 22:15:27 +0000

    4th draft

--- app/controllers/foods_recipes_controller.rb
+++ app/controllers/foods_recipes_controller.rb
@@ -1,30 +1,49 @@
 class FoodsRecipesController < ApplicationController
   def index
-    @foods_recipes = FoodsRecipe.all
+    @q = FoodsRecipe.ransack(params[:q])
+    @foods_recipes = @q.result(:distinct => true).includes(:foods, :recipes).page(params[:page]).per(10)
+
+    render("foods_recipes/index.html.erb")
   end
 
   def show
     @foods_recipe = FoodsRecipe.find(params[:id])
+
+    render("foods_recipes/show.html.erb")
   end
 
   def new
     @foods_recipe = FoodsRecipe.new
+
+    render("foods_recipes/new.html.erb")
   end
 
   def create
     @foods_recipe = FoodsRecipe.new
+
     @foods_recipe.foods_id = params[:foods_id]
     @foods_recipe.recipes_id = params[:recipes_id]
 
-    if @foods_recipe.save
-      redirect_to "/foods_recipes", :notice => "Foods recipe created successfully."
+    save_status = @foods_recipe.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/foods_recipes/new", "/create_foods_recipe"
+        redirect_to("/foods_recipes")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Foods recipe created successfully.")
+      end
     else
-      render 'new'
+      render("foods_recipes/new.html.erb")
     end
   end
 
   def edit
     @foods_recipe = FoodsRecipe.find(params[:id])
+
+    render("foods_recipes/edit.html.erb")
   end
 
   def update
@@ -33,10 +52,19 @@ class FoodsRecipesController < ApplicationController
     @foods_recipe.foods_id = params[:foods_id]
     @foods_recipe.recipes_id = params[:recipes_id]
 
-    if @foods_recipe.save
-      redirect_to "/foods_recipes", :notice => "Foods recipe updated successfully."
+    save_status = @foods_recipe.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/foods_recipes/#{@foods_recipe.id}/edit", "/update_foods_recipe"
+        redirect_to("/foods_recipes/#{@foods_recipe.id}", :notice => "Foods recipe updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Foods recipe updated successfully.")
+      end
     else
-      render 'edit'
+      render("foods_recipes/edit.html.erb")
     end
   end
 
@@ -45,6 +73,10 @@ class FoodsRecipesController < ApplicationController
 
     @foods_recipe.destroy
 
-    redirect_to "/foods_recipes", :notice => "Foods recipe deleted."
+    if URI(request.referer).path == "/foods_recipes/#{@foods_recipe.id}"
+      redirect_to("/", :notice => "Foods recipe deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Foods recipe deleted.")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root :to => "foods#index"
   # Routes for the Foods_recipe resource:
